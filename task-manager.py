36f7167f8de162812643fb42df9adfb9751ba7d0
Anna Siryak <grafova.a.i@gmail.com>
2025-05-18 18:35:07 +0300

    add class & __init__
    добавила класс и инициализировала его

--- task-manager.py
+++ task-manager.py
@@ -1,1 +1,9 @@
 # Задача: Создай класс Task, который позволяет управлять задачами (делами). У задачи должны быть атрибуты: описание задачи, срок выполнения и статус (выполнено/не выполнено). Реализуй функцию для добавления задач, отметки выполненных задач и вывода списка текущих (не выполненных) задач.
+
+class Task():
+
+    def __init__(self, name, description, deadline=None, status=False):
+        self.name = name
+        self.description = description
+        self.deadline = deadline
+        self.status = status
